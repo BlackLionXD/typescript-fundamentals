4a1b476a7bb2762177185e9461fd884827030770
Ibrahim Gidi <114783401+BlackLionXD@users.noreply.github.com>
2024-05-26 12:31:49 +0300

    4 interface

---
+++ interface.ts
@@ -0,0 +1,20 @@
+
+
+interface Person{
+    name: string,
+    age: number,
+    salary?: number,
+    isAdult?: boolean
+}
+
+let person1: Person = {
+    name: 'Ibro',
+    age: 25,
+    salary: 10000,
+    isAdult: true
+}
+
+let person2: Person = {
+    name: 'Charlie',
+    age: 38
+}
