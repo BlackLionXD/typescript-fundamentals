9603a0b44230843ba70cdd8f26cf9726b1b2c295
Ibrahim Gidi <114783401+BlackLionXD@users.noreply.github.com>
2024-05-26 11:21:23 +0300

    2 decaring variables

---
+++ log.js
@@ -0,0 +1,20 @@
+function log(message) {
+    console.log(message);
+}
+// variable declaration
+// 1 implicit declaration 
+var x = 'sdjflf';
+log(x);
+// 2 explicit declaration
+var num = 12;
+var str = 'sdfsdf';
+var numArray = [12, 54, 66];
+var truthValue = false;
+// 3 tuple declaration
+var tuple = ["Ibro", 25, true];
+// 4 object declaration
+var person = {
+    name: "Ibrahim", age: 25, isAdmin: true
+};
+//  5 union types 
+var password = "sjdof44";
---
+++ log.ts
@@ -0,0 +1,24 @@
+
+
+// variable declaration
+// 1 implicit declaration 
+let x = 'sdjflf';
+// 2 explicit declaration
+
+let num: number = 12;
+let str: string = 'sdfsdf';
+let numArray: number[] = [12,54,66];
+let truthValue:boolean = false;
+
+// 3 tuple declaration
+let tuple: [string, number, boolean]= [ "Ibro",25,true];
+
+// 4 object declaration
+
+let person: { name:string, age: number, isAdmin: boolean}= {
+    name : "Ibrahim", age : 25, isAdmin: true
+}
+
+//  5 union types 
+
+let password: string | number = "sjdof44";
